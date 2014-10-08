2b385c4b1b7355b9fa080e3f0955f0d1e406e00b
MorenoB <moreno_bralts@hotmail.com>
2014-10-08 15:26:03 +0200

    Fixed Spelling error!!

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         for(;;)
-            System.out.println("HERPDERP");
+            System.out.println("HERDERS");
     }
 }
