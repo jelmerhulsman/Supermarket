864dd6295db229844435133174f2398bcee9bad7
megagorilla <sanderenbart@gmail.com>
2014-10-21 13:50:47 +0200

    added Storage en Truck

--- src/supermarket/Truck.java
+++ src/supermarket/Truck.java
@@ -1,3 +1,56 @@
+package supermarket;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Truck {
 
+    public enum Status {
+        WAITING, UNLOADING, AWAY, EMPTY
+    };
+    private List<Item> items = new ArrayList<Item>();
+    private Status status;
+
+    /**
+     * creates a truck
+     * @param status WAITING, UNLOADING, AWAY, EMPTY
+     */
+    public Truck(Status status) {
+        this.status = status;
+    }
+
+    /**
+     * returns the status of the truck
+     * @return WAITING, UNLOADING, AWAY, EMPTY
+     */
+    public Status getStatus() {
+        return status;
+    }
+
+    /**
+     * sets the status of the truck
+     * @param status WAITING, UNLOADING, AWAY, EMPTY
+     */
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    /**
+     * unloads the truck (returns all the items in the truck in an array
+     *                    and removes them from the truck)
+     * @return items
+     */
+    public List<Item> unload() {
+        List<Item> bufferItems = items;
+        items.clear();
+        return bufferItems;
+    }
+
+    /**
+     * orders items (add's items to the truck)
+     * @param items 
+     */
+    public void order(List<Item> items) {
+        this.items.add(items);
+    }
 }
