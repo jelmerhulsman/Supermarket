c48757ad718c036876380f37d565f3bd478786cd
MorenoB <moreno_bralts@hotmail.com>
2014-10-21 15:39:06 +0200

    Department

--- src/supermarket/Department.java
+++ src/supermarket/Department.java
@@ -1,5 +1,35 @@
 package supermarket;
 
+import java.util.ArrayList;
+
 public class Department {
 
+    private String name;
+    private ArrayList<Aisle> aisles;
+
+    public Department(String name) {
+        this.name = name;
+    }
+/**
+ * Gets the name of this department
+ * @return the name of the department
+ */
+    public String getName() {
+        return name;
+    }
+/**
+ * Gets all the containing Aisles in this department
+ * @return an ArrayList of aisles
+ */
+    public ArrayList<Aisle> getAisles() {
+        return aisles;
+    }
+
+    /**
+     * Sets the aisles in this department
+     * @param aisles The aisles you are willing to set to this department
+     */
+    public void setAisles(ArrayList<Aisle> aisles) {
+        this.aisles = aisles;
+    }
 }
