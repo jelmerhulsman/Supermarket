45144e42b0f5b5f8413a0a8040e398aa7af524bb
MorenoB <moreno_bralts@hotmail.com>
2014-10-21 15:44:44 +0200

    JEZUS WAT VEEL GETYP!

--- src/supermarket/StaffTypes/Staff.java
+++ src/supermarket/StaffTypes/Staff.java
@@ -2,4 +2,18 @@ package supermarket.StaffTypes;
 
 public class Staff {
 
+    private String name;
+
+    private Staff(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Gets the name of this Staff member
+     *
+     * @return the name of this staff member
+     */
+    public String getName() {
+        return name;
+    }
 }
