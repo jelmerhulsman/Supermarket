e40852f998f3325d22af4b7bb3cb62cada4543b2
MorenoB <moreno_bralts@hotmail.com>
2014-10-22 13:37:41 +0200

    IK MOEST SYNCEN

--- src/supermarket/StaffTypes/Unloader.java
+++ src/supermarket/StaffTypes/Unloader.java
@@ -1,11 +1,15 @@
 package supermarket.StaffTypes;
 
+import supermarket.Storage;
+
 public class Unloader extends Staff {
 
-    
+     private Storage storage;
     public Unloader(String name)
     {
+       
         super(name);
+        this.objectType = storage;
     }
     
     
