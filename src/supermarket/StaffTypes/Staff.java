9bf20c4167fcb58e883e515fd2e19e3732ec3f03
DylanDavids <dylan.davids@live.nl>
2014-10-22 10:52:59 +0200

    Ik moet kommiten van git :(

--- src/supermarket/StaffTypes/Staff.java
+++ src/supermarket/StaffTypes/Staff.java
@@ -1,5 +1,14 @@
 package supermarket.StaffTypes;
 
-public class Staff {
+import java.util.List;
 
+public class Staff {
+    private String name;
+    private List<Staff> functions;
+    private Staff curFunctions;
+    
+    public Staff(String name, List<Staff> functions){
+        this.name = name;
+        this.functions = functions;
+    }
 }
