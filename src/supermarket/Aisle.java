0ca4d96305a392b61ceb5b01be6e99414ce3041e
Jelmer <jelmer_hulsman@hotmail.com>
2014-10-22 12:06:43 +0200

    Customer constructor now working
    (Still have to change/add items on blacklist and liking list)

--- src/supermarket/Aisle.java
+++ src/supermarket/Aisle.java
@@ -1,25 +1,33 @@
 package supermarket;
 
-import java.util.ArrayList;
+import java.util.List;
 import supermarket.Item.Category;
 import supermarket.StaffTypes.AisleLoader;
 
 public class Aisle {
-
-    private String name;
-    private ArrayList<Item> items = new ArrayList();
-    private AisleLoader currentLoader;
-
+    
+    private final int ITEM_LIMIT_AISLE = 50;
+    
     public enum Status {
 
-        FULL, EMPTY, NORMAL
+        EMPTY, STOCKED, FULL
     }
+    
+    private String name;
     private Status status;
-    private final int ITEMLIMIT = 50;
-    private ArrayList<Category> categories = new ArrayList();
+    private AisleLoader stocker;
+    private List<Category> categories;
+    private List<Item> items;
+    private List<Customer> customers;
 
     public Aisle(String name, Category category1, Category category2) {
         this.name = name;
+        status = Status.EMPTY;
+        stocker = null;
+        categories = null;
+        items = null;
+        customers = null;
+        
         categories.add(category1);
         categories.add(category2);
     }
@@ -39,7 +47,7 @@ public class Aisle {
      * @param currentLoader The loader that you are willing to assign
      */
     public void setCurrentLoader(AisleLoader currentLoader) {
-        this.currentLoader = currentLoader;
+        this.stocker = currentLoader;
     }
 
     /**
@@ -48,7 +56,7 @@ public class Aisle {
      * @return the current loader
      */
     public AisleLoader getCurrentLoader() {
-        return currentLoader;
+        return stocker;
     }
 
     /**
@@ -56,7 +64,7 @@ public class Aisle {
      *
      * @return a list of all the items
      */
-    public ArrayList<Item> getItems() {
+    public List<Item> getItems() {
         return items;
     }
 
@@ -65,7 +73,7 @@ public class Aisle {
      *
      * @return two categories that are assigned to this Aisle in a list
      */
-    public ArrayList<Category> getCategories() {
+    public List<Category> getCategories() {
         return categories;
     }
 
@@ -95,12 +103,12 @@ public class Aisle {
      * being empty and sets the Status accordingly.
      */
     public void checkAisle() {
-        if (items.size() == ITEMLIMIT) {
+        if (items.size() == ITEM_LIMIT_AISLE) {
             status = Status.FULL;
         } else if (items.isEmpty()) {
             status = Status.EMPTY;
         } else {
-            status = Status.NORMAL;
+            status = Status.STOCKED;
         }
     }
 }
--- src/supermarket/Item.java
+++ src/supermarket/Item.java
@@ -1,22 +1,33 @@
 package supermarket;
 
 public class Item {
+
+    public enum Category {
+
+        WINE, SNACK, SODA, BEER, LIQUOR, DAIRY, SPICYFOOD, NONSPICYFOOD, FROZENFOOD, CANNEDFOOD, FRESH, VEGTABLES
+    }
+
+    public enum Status {
+
+        LOADED, INSTORAGE, ONCUSTOMER, ONSTAFF, INTRUCK
+    }
     private String name;
-    public enum Category  { WINE, SNACK, SODA, BEER, LIQUOR, DIARY, SPICYFOOD, NONSPICYFOOD, FROZENFOOD, CANNEDFOOD }
-    public enum Status { LOADED, INSTORAGE, ATCUSTOMER, ATSTAFF, INTRUCK  } 
     private float price;
+    private boolean primary;
     private Status status;
     private Category category;
-    
-    public Item(String name, Category category, float price){
+
+    public Item(String name, Category category, float price, boolean primary) {
         this.name = name;
         this.price = price;
+        this.primary = primary;
         this.category = category;
         this.status = Status.INTRUCK;
     }
 
     /**
      * Gets the Status of the Item
+     *
      * @return wether the item is loaded, at a customer, purchased and whatever
      */
     public Status getStatus() {
@@ -25,14 +36,25 @@ public class Item {
 
     /**
      * Gets the current category of this item
+     *
      * @return The current category
      */
     public Category getCategory() {
         return category;
     }
+    
+    /**
+     * Returns the primary status of this item
+     *
+     * @return The primary status of this item
+     */
+    public boolean isPrimary() {
+        return primary;
+    }
 
     /**
      * Gets the current price for the customer
+     *
      * @return The current price for the customer
      */
     public float getPrice() {
@@ -41,6 +63,7 @@ public class Item {
 
     /**
      * Gets the name of this item
+     *
      * @return The name of this item
      */
     public String getName() {
@@ -49,6 +72,7 @@ public class Item {
 
     /**
      * Sets the category of this item to something else
+     *
      * @param category The new category for this item
      */
     public void setCategory(Category category) {
@@ -57,10 +81,11 @@ public class Item {
 
     /**
      * Sets the status of this item to something else
-     * @param status wether the item is loaded, at a customer, purchased and whatever
+     *
+     * @param status wether the item is loaded, at a customer, purchased and
+     * whatever
      */
     public void setStatus(Status status) {
         this.status = status;
     }
-    
 }
