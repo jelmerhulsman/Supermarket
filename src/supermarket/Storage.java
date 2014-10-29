a73e0108efb4c94bde4fc4a72d8cf25c31289661
MorenoB <moreno_bralts@hotmail.com>
2014-10-29 15:58:55 +0100

    hibernatuuuu

--- src/supermarket/Storage.java
+++ src/supermarket/Storage.java
@@ -99,7 +99,6 @@ public class Storage extends ObjectInShop {
      * @return the list of the items
      */
     public ArrayList<Item> moveItem(Item.Category cat, int amount){
-        ArrayList<Item> items = new ArrayList<Item>();
         ArrayList<Item> allItems = getItems();
         int counter = 0;
         for(int i = 0; i<allItems.size();i++){
