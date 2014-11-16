b5aec3721c39ae6a37257ca1d909c8423d361c90
MorenoB <moreno_bralts@hotmail.com>
2014-11-16 15:36:22 +0100

    Author veranderd

--- src/supermarket/Sales.java
+++ src/supermarket/Sales.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package supermarket;
 
 import com.jme3.math.Vector2f;
@@ -9,12 +5,11 @@ import java.util.ArrayList;
 
 /**
  *
- * @author Dylankuyjt67hr
+ * @author SDJM
  */
 public class Sales extends Aisle {
-    
-    public Sales(String name, Vector2f location, ArrayList<Item.Category> categories, ArrayList<Item> storeItems){
+
+    public Sales(String name, Vector2f location, ArrayList<Item.Category> categories, ArrayList<Item> storeItems) {
         super(name, location, categories, storeItems);
     }
-    
 }
